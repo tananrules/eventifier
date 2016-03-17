4ff7d3dcdd9f341c705132a9fcc7391b0d8826c0
Tarun Arora <tananrules@gmail.com>
2016-03-18 03:14:43 +0530

    masonry and imagesloaded added

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,9 @@ module.exports = function(grunt) {
                 'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
                 'vendor/angular-ui-router/release/angular-ui-router.js',
                 'vendor/angular-ui-utils/modules/route/route.js',
-                'vendor/instafeed.js/instafeed.min.js'
+                'vendor/instafeed.js/instafeed.min.js',
+                'vendor/masonry/dist/masonry.pkgd.js',
+                'vendor/imagesloaded/imagesloaded.pkgd.js'
             ],
             css: [
             ],
--- src/app/instagram/instagram.js
+++ src/app/instagram/instagram.js
@@ -1,33 +1,42 @@
 (function(app) {
 
     app.controller('InstagramController', function ($scope) {
-        // var tag = $scope.search;
-        var model = this;
-        model.searchTag = loadInstagram;
+      // var tag = $scope.search;
+      var model = this;
+      model.searchTag = loadInstagram;
 
-        init();
+      init();
 
-        function init() {
-            // A definitive place to put everything that needs to run when the controller starts. Avoid
-            //  writing any code outside of this function that executes immediately.
-        }
+      function init() {
+          // A definitive place to put everything that needs to run when the controller starts. Avoid
+          //  writing any code outside of this function that executes immediately.
+      }
 
-        function loadInstagram(tagName){
-          var feed = new Instafeed({
-                      get: 'tagged',
-                      tagName: tagName,
-                      accessToken: '6678174.467ede5.205a03ebc4b74d4082823781c3149575',
-                      target: 'instafeed',
-                      sortBy: 'most-recent',
-                      limit: 33,
-            // template: '<a title="{{caption}}" class="fancybox" rel="group" href="{{image}}"><img src="{{image}}" title="{{caption}}" /><div id="filter">{{model.filter}}</div><div class="info"><p class="location" ng-model="location"><i class="icon-location"></i>{{location}}</p><p class="user"><i class="icon-user"></i>@{{model.user.username}}</p><p><i class="icon-comment"></i>{{caption}}</p><br><ul><li class="icon-heart">{{likes}} likes<li class="icon-chat">{{comments}} comments</ul></div></a>',
-            // after: function() {
-            //   if (!this.hasNext()) {
-            //     loadButton.setAttribute('disabled', 'disabled');
-            //   }
-            // }
-          }).run();
-        }
+      function loadInstagram(tagName){
+        var feed = new Instafeed({
+            get: 'tagged',
+            tagName: tagName,
+            accessToken: '6678174.467ede5.205a03ebc4b74d4082823781c3149575',
+            target: 'instafeed',
+            sortBy: 'most-recent',
+            limit: 33,
+            resolution: 'low_resolution',
+            template: '<div class="item"><img src="{{image}}"/></div>',
+          
+          after: function() {
+            var elem = document.querySelector('#instafeed');
+
+            var msnry; 
+
+            imagesLoaded(elem, function() {
+              msnry = new Masonry(elem, {
+                itemSelector: '.item',
+                columnWidth: 320,
+              });
+            });
+          }
+        }).run();
+      }
 
 
 
